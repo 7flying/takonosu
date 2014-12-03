fb3618d19f124ce4cda27a73cfa23acec4e3ba04
7flying <diez.f.irene@gmail.com>
2014-12-03 17:51:10 +0100

    redirect to index 2

--- takonosu/app/routes/index.py
+++ takonosu/app/routes/index.py
@@ -1,4 +1,7 @@
 from app import app
+from flask import abort, redirect, url_for
+
+"""
 from werkzeug.routing import BaseConverter
 
 class RegexConverter(BaseConverter):
@@ -7,7 +10,20 @@ class RegexConverter(BaseConverter):
 		self.regex = items[0]
 
 app.url_map.converters['regex'] = RegexConverter
+"""
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # return app.send_static_file('index.html')
+    return redirect(url_for('.index'))
+"""
+
+@app.route('/<regex(".+"):else>/')
+def everything_else():
+	return app.send_static_file('index.html')
+
+"""
